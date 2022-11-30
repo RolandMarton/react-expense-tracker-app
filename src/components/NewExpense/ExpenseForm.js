5c9eece2a71567282a2a2105397a8fda82197902
RolandMarton <roland111187@gmail.com>
2022-11-30 15:41:22 +0100

    Add Chart to show the highest month of a year

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseDate = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
